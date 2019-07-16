46285b5c9cd320ccaab49ad24c633b9890fad8f5
madebyrh <madebyrh@gmail.com>
2019-07-16 22:54:00 +0900

    画面遷移をpush => pushNamedに変更

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   // 他画面からデータ受取り
-  final String categoryId;
-  final String categoryTitle;
-  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  // main.dartのroutesを使用する場合は不要
+  // final String categoryId;
+  // final String categoryTitle;
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
   @override
   Widget build(BuildContext context) {
+    // pushNamedのargumentsの値を利用する
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
