b8ecef5ecfb1f7962bbfc5c555ba21232082a4cd
madebyrh <madebyrh@gmail.com>
2019-07-16 22:41:35 +0900

    画面間の値の受け渡し処理追加

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  // 他画面からデータ受取り
+  final String categoryId;
+  final String categoryTitle;
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('The Recipes For The Category!'),
