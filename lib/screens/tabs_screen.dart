1acffc3e6d0ebc8ee0dda6ec6a1635009854886e
madebyrh <madebyrh@gmail.com>
2019-07-19 00:15:53 +0900

    change tab bar position from top to bottom

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -7,38 +7,89 @@ class TabsScreen extends StatefulWidget {
   _TabsScreenState createState() => _TabsScreenState();
 }
 
+// 画面上部のタブ
+// class _TabsScreenState extends State<TabsScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       initialIndex: 0, //どのページをスタートにするか
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Meals'),
+//           bottom: TabBar(
+//             tabs: <Widget>[
+//               Tab(
+//                 icon: Icon(
+//                   Icons.category,
+//                 ),
+//                 text: 'Categories',
+//               ),
+//               Tab(
+//                 icon: Icon(
+//                   Icons.star,
+//                 ),
+//                 text: 'Favorites',
+//               ),
+//             ],
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: <Widget>[
+//             CategoriesScreen(),
+//             FavoritesScreen(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// 画面下部のタブ
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Favorites',
+    },
+  ];
+  int _selectedPageIndex = 0;
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0, //どのページをスタートにするか
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: 'Favorites',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage, //引数のindexは自動的に
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex, //これがあると選んだ箇所の色が変わる
+        type: BottomNavigationBarType.fixed, //画面遷移の時のタブのアニメーション設定
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
       ),
     );
   }
