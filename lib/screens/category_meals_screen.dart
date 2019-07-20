8dc2ff1fc416162511d3f642b0d68a436f9082c4
madebyrh <madebyrh@gmail.com>
2019-07-20 12:30:13 +0900

    add delete function

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,24 +1,78 @@
 import 'package:flutter/material.dart';
 import '../dummy_data.dart';
 import '../widgets/meal_item.dart';
+import '../models/meal.dart';
 
-class CategoryMealsScreen extends StatelessWidget {
+class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
-  // 他画面からデータ受取り
-  // main.dartのroutesを使用する場合は不要
-  // final String categoryId;
-  // final String categoryTitle;
-  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  var _loadedInitData = false;
+  @override
+  void initState() {
+    // didChangeDependensiesに移動
+    // final routeArgs =
+    //     ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    // // initStateは画面を作る前に動くので、contextが使えない
+    // final categoryId = routeArgs['id'];
+    // //final categoryTitle = routeArgs['title'];
+    // categoryTitle = routeArgs['title'];
+
+    // // final categoryMeals = DUMMY_MEALS.where((meal) {
+    // //   return meal.categories.contains(categoryId);
+    // // }).toList();
+    // displayedMeals = DUMMY_MEALS.where((meal) {
+    //   return meal.categories.contains(categoryId);
+    // }).toList();
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    if (!_loadedInitData) {
+      final routeArgs =
+          ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+      // initStateは画面を作る前に動くので、contextが使えない
+      final categoryId = routeArgs['id'];
+      //final categoryTitle = routeArgs['title'];
+      categoryTitle = routeArgs['title'];
+
+      // final categoryMeals = DUMMY_MEALS.where((meal) {
+      //   return meal.categories.contains(categoryId);
+      // }).toList();
+      displayedMeals = DUMMY_MEALS.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // pushNamedのargumentsの値を利用する
-    final routeArgs =
-        ModalRoute.of(context).settings.arguments as Map<String, String>;
-    final categoryId = routeArgs['id'];
-    final categoryTitle = routeArgs['title'];
-    final categoryMeals = DUMMY_MEALS.where((meal) {
-      return meal.categories.contains(categoryId);
-    }).toList();
+    // final routeArgs =
+    //     ModalRoute.of(context).settings.arguments as Map<String, String>;
+    // final categoryId = routeArgs['id'];
+    // final categoryTitle = routeArgs['title'];
+    // final categoryMeals = DUMMY_MEALS.where((meal) {
+    //   return meal.categories.contains(categoryId);
+    // }).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -28,15 +82,16 @@ class CategoryMealsScreen extends StatelessWidget {
         itemBuilder: (ctx, index) {
           //return Text(categoryMeals[index].title);
           return MealItem(
-            id: categoryMeals[index].id,
-            title: categoryMeals[index].title,
-            imageUrl: categoryMeals[index].imageUrl,
-            duration: categoryMeals[index].duration,
-            complexity: categoryMeals[index].complexity,
-            affordability: categoryMeals[index].affordability,
+            id: displayedMeals[index].id,
+            title: displayedMeals[index].title,
+            imageUrl: displayedMeals[index].imageUrl,
+            duration: displayedMeals[index].duration,
+            complexity: displayedMeals[index].complexity,
+            affordability: displayedMeals[index].affordability,
+            removeItem: _removeMeal,
           );
         },
-        itemCount: categoryMeals.length,
+        itemCount: displayedMeals.length,
       ),
     );
   }
